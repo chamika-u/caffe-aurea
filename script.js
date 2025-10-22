ecc7b1cade85a9fa1f8a93c707fdd123073d818e
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-10-22 14:59:25 +0530

    updated js for slider

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ menuOpenButton.addEventListener('click', () => {
 // close menu when the close button clicked
 menuCloseButton.addEventListener('click', () => menuOpenButton.click
 ());
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+
+  },
+
+});
+
